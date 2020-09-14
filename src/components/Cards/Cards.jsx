a0b3b2b88783979ea5aeb7b2bdc73034b7cf8199
ahhzaky <siahmadzaky@gmail.com>
2020-09-14 23:48:54 +0700

    call data from the API

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -3,8 +3,11 @@ import { Card, CardContent, Typography, Grid } from "@material-ui/core";
 
 import styles from "./Cards.module.css";
 
-const Cards = (props) => {
-  console.log("Cards>>", props);
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+  if (!confirmed) {
+    return "Loading";
+  }
+
   return (
     <div className={styles.container}>
       <Grid container spacing={3} justify="center">
@@ -13,10 +16,36 @@ const Cards = (props) => {
             <Typography color="textSecondary" gutterBottom>
               Infected
             </Typography>
-            <Typography varaint="h5">REAL DATA</Typography>
+            <Typography varaint="h5">{confirmed.value}</Typography>
             <Typography color="textSecondary">REAL DATA</Typography>
             <Typography varaint="body2">
               Number of active cases of COVID-19
+            </Typography>
+          </CardContent>
+          <Grid />
+        </Grid>
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Recovered
+            </Typography>
+            <Typography varaint="h5">REAL DATA</Typography>
+            <Typography color="textSecondary">REAL DATA</Typography>
+            <Typography varaint="body2">
+              Number of recoveries from COVID-19
+            </Typography>
+          </CardContent>
+          <Grid />
+        </Grid>
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Deaths
+            </Typography>
+            <Typography varaint="h5">REAL DATA</Typography>
+            <Typography color="textSecondary">REAL DATA</Typography>
+            <Typography varaint="body2">
+              Number of deaths caused by COVID-19
             </Typography>
           </CardContent>
           <Grid />
