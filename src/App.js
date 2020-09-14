804294dabb2103635d3c219ef0e1a4563ad83a7a
ahhzaky <siahmadzaky@gmail.com>
2020-09-14 23:39:39 +0700

    Setup Cards{cards@material-ui}

--- src/App.js
+++ src/App.js
@@ -5,15 +5,22 @@ import styles from "./App.module.css";
 import { fetchData } from "./api";
 
 class App extends React.Component {
+  state = {
+    data: {},
+  };
+
   async componentDidMount() {
-    const data = await fetchData();
-    console.log(data);
+    const fetchedData = await fetchData();
+
+    this.setState({ data: fetchedData });
   }
 
   render() {
+    const { data } = this.state;
+
     return (
       <div className={styles.container}>
-        <Cards />
+        <Cards data={data} />
         <CountryPicker />
         <Chart />
       </div>
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { Card, CardContent, Typography, Grid } from "@material-ui/core";
 
-const Cards = () => {
+import styles from "./Cards.module.css";
+
+const Cards = (props) => {
+  console.log("Cards>>", props);
   return (
-    <div>
-      <h1>Cards</h1>
+    <div className={styles.container}>
+      <Grid container spacing={3} justify="center">
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Infected
+            </Typography>
+            <Typography varaint="h5">REAL DATA</Typography>
+            <Typography color="textSecondary">REAL DATA</Typography>
+            <Typography varaint="body2">
+              Number of active cases of COVID-19
+            </Typography>
+          </CardContent>
+          <Grid />
+        </Grid>
+      </Grid>
     </div>
   );
 };
